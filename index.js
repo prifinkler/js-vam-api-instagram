60eb467d2a9dab90486c2c290e9ce3103cf74dfd
Priscila Finkler <prifinkler@gmail.com>
2024-10-03 15:13:52 +0100

    integrated museum api

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21,
-        isLiked: false
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4,
-        isLiked: false
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152,
-        isLiked: false
-    }
-]
+const url = "https://api.vam.ac.uk/v2/objects/search?q=queen&page_size=50"
+let artPosts = [];
+
+const fetchArtData = async () => {
+
+  try {
+    const res = await axios.get(url);
+    const results = res.data.records;
+
+    const arts = await Promise.all(results.map(async (item) => {
+      const image_url = `https://framemark.vam.ac.uk/collections/${item._primaryImageId}/square/full/0/default.jpg`;
+      const description_url = `https://iiif.vam.ac.uk/collections/${item.systemNumber}/manifest.json`;
+
+      try {
+        const descriptionResponse = await axios.get(description_url);
+        const descriptionData = descriptionResponse.data.description
+
+        return {
+          id: item.systemNumber,
+          maker: formatName(item._primaryMaker?.name) || 'Unknown',
+          location: item._primaryPlace || 'Unknown',
+          post: image_url,
+          description: descriptionData,
+          likes: Math.floor(Math.random() * 1000),
+          isLiked: false
+        };
+      } catch (error) {
+        console.error(`Error fetching description for item: ${item.systemNumber}`, error);
+        return null;
+      }
+    }));
+
+    artPosts = arts.filter(art => art !== null);
+    return artPosts
+  } catch (error) {
+    console.error("Error fetching data:", error)
+    return [];
+  }
+};
+
+function formatName(name) {
+  const nameParts = name.split(', ');
+  const formattedName = nameParts.length > 1 ? `${nameParts[1]} ${nameParts[0]}` : name
+  return formattedName
+}
 
 
 function createPost(post, index) {
   const postElement = document.createElement("section")
 
   postElement.innerHTML = `
-          <div class="user-info">
-            <img src="${post.avatar}" class="avatar" alt="User avatar">
-            <div>
-              <h1>${post.name}</h1>
-              <p>${post.location}</p>
-            </div>
-          </div>
-          <img src="${post.post}" class="post-image" alt="User post image">
-          <div class="post-body">
-            <div class="icons">
-              <button onclick="likePost(${index})"><i class="fa-heart fa-regular"></i></button>
-              <button><i class="fa-regular fa-comment"></i></button>
-              <button><i class="fa-regular fa-paper-plane"></i></button>
-            </div>
-            <div class="like-count">${post.likes} likes</div>
-            <div class="username-caption">
-              <p><span>${post.username}</span> ${post.comment}</p>
-            </div>
-          </div>
-  `;
+      <div class="user-info">
+        <img src="${post.post}" class="avatar" alt="User avatar">
+        <div>
+          <h1>${post.maker}</h1>
+          <p>${post.location}</p>
+        </div>
+      </div>
+      <img src="${post.post}" class="post-image" alt="User post image">
+      <div class="post-body">
+        <div class="icons">
+          <button onclick="likePost(${index})" type="button" aria-label="like button"><i class="fa-heart fa-regular"></i></button>
+          <button type="button" aria-label="comment button"><i class="fa-regular fa-comment"></i></button>
+          <button type="button" aria-label="forward button"><i class="fa-regular fa-paper-plane"></i></button>
+        </div>
+        <div class="like-count">${post.likes} likes</div>
+        <div class="username-caption">
+          <p><span>${post.maker}</span> ${post.description}</p>
+        </div>
+      </div>
+    `;
 
   return postElement;
 }
 
-function renderAllPosts() {
+async function renderAllPosts() {
   const main = document.querySelector("main");
-  main.innerHTML = '';
-  posts.forEach((post, index) => {
+  main.innerHTML = '<p>Loading...</p>';
+
+  await fetchArtData();
+
+  main.innerHTML ='';
+  artPosts.forEach((post, index) => {
     main.appendChild(createPost(post, index))
   })
 }
 
-
 function likePost(index) {
-  const post = posts[index]
+  const post = artPosts[index]
   post.isLiked = !post.isLiked
   post.likes += post.isLiked ? +1 : -1;
 
@@ -82,4 +98,4 @@ function likePost(index) {
   likesEl.textContent = `${post.likes} likes`
 }
 
-renderAllPosts();
+window.onload = renderAllPosts;
